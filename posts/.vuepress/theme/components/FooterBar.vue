c6cc8b73a74c5d81b0d5c3a5004b366a48b9a583
Nicolas Beauvais <nicolas@bvs.email>
2023-03-13 10:48:07 +0100

    Update copy

--- posts/.vuepress/theme/components/FooterBar.vue
+++ posts/.vuepress/theme/components/FooterBar.vue
@@ -3,7 +3,7 @@
     <p>
       © {{ thisYear }} · {{ author }} · <a href="https://github.com/nicolasbeauvais">GitHub</a> · <a href="https://twitter.com/w3Nicolas">Twitter</a>
     </p>
-    <p>Support my work with <a href="https://minkit.io/">MinKit active monitoring</a>.</p>
+    <p>Support my work with <a href="https://minkit.io/">MinKit website monitoring</a>.</p>
   </footer>
 </template>
 
